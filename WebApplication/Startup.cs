1c775b1fab4ced8cde5cb48060507e2d3f203b68
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2020-08-17 15:13:08 -0400

    disable warning from Rider

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -15,6 +15,7 @@ namespace WebApplication
             Configuration = configuration;
         }
 
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
